158ee75b7216a1b4a18e05fea54440edfa6c5c4d
Ahmed Samir <ahmedsamirf11@gmail.com>
2022-05-08 16:01:37 +0200

    some packages added and project base structure is done

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public int NumberInStock { get; set; }
+        public float Price { get; set; }
+        public float Rate { get; set; }
+    }
+}
