4ca1a403bd97dd17dfec93d6a2310472cd92a031
Ahmed Samir <ahmedsamirf11@gmail.com>
2022-05-26 15:34:11 +0200

    add auth to order

--- ShopperAPi/Controllers/OrderController.cs
+++ ShopperAPi/Controllers/OrderController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ShopperAPi.DTOS.OrderDto;
 using ShopperAPi.Errors;
+using ShopperAPi.Helpers;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -49,16 +50,17 @@ namespace ShopperAPi.Controllers
         [HttpPost]
         public async Task<IActionResult> Post(UserOrderDto userOrder)
         {
-            string basketId = "sss";
+            var _currentUserData = User.Claims.getCurrentUserIdAndEmail();
+            var basketId = _currentUserData.Item1.hashStrings(_currentUserData.Item2);
             var isProductAvalible = await _orderService.isProductsAvalibleAync(basketId);
             if (!isProductAvalible.Item1) {
                 isProductAvalible.Item2.ForEach(e =>
                 {
-                    ModelState.AddModelError(e, e);
+                    ModelState.AddModelError("",e);
                 });
                 return BadRequest(ModelState);
             }
-            var order = _orderService.createOrderAsync(_mapper.Map<Address>(userOrder.Address)
+            var order = await _orderService.createOrderAsync(_mapper.Map<Address>(userOrder.Address)
                 ,userOrder.UserEmail,basketId);
             _orderService.saveChanges();
             return Created("", order);
